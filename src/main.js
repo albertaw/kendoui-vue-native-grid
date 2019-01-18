1d6ff242020a9ae0e381c98fefd32cb382d97d06
Alberta Williams <alberta04@gmail.com>
2019-01-17 19:07:15 -0500

    update imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Grid } from '@progress/kendo-vue-grid'
 import '@progress/kendo-theme-default/dist/all.css'
+import { Grid } from '@progress/kendo-vue-grid'
 
 Vue.component('Grid', Grid);
 
